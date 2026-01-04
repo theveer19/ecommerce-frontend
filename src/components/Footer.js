db507056f73a3aabc490998ccb4ed6e06e1b3db0
theveer19 <veergurjar2002@gmail.com>
2026-01-04 08:58:52 +0530

    Update frontend with live Razorpay configuration

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom"; 
 import { Box, Container, Grid, Typography, InputBase, IconButton, Divider } from "@mui/material";
 // 1. Import Linkedin
